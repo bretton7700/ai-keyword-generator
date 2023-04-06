af6792f26cf23b0c982f053152aef08db9cfe504
gitonga bretton <gitongabretton@gmail.com>
2023-04-06 16:04:04 +0300

    error pop up

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -3,9 +3,21 @@ import { Textarea, Button, useToast } from '@chakra-ui/react'
 
 const TextInput = () => {
     const [text, setText] = useState('');
+    const toast = useToast();
 
     const submitText = () =>{
-        
+        if(text === ''){
+            toast({
+                title: 'Text field is empty',
+                description: 'Please enter some text to get keywords',
+                status: 'error',
+                duration: 5000,//5 seconds or 5000 milliseconds
+                isClosable: false //making it to have an x at the top
+            })
+        }else{
+            console.log(text)
+        }
+
     }
   return (
     <>

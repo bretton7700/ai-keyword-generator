6553b86dfa8b96104323c2ca2fcbfe0b40bda72e
gitonga bretton <gitongabretton@gmail.com>
2023-04-06 14:52:48 +0300

    base styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-
+import { Container, Box } from "@chakra-ui/react"
 const App = () => {
   return (
-    <>
+    <Box bg ='blue.600' color='white' height='100vh' paddingTop={130}>
 
-    <h1>My App</h1>
-    
-    </>
+      <Container maxW='3xl' centerContent>
+      <h1>My App</h1>
+      </Container>
+     
+    </Box>
   )
 }
 
